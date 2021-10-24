d048fbae814b6956903c3c3819a039977d3df963
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-24 15:39:33 -0500

    Move to config file

--- src/app/shared/apis/index.ts
+++
@@ -1,5 +0,0 @@
-export {
-  YTB_API_KEY,
-  YTB_API_URL,
-  YTB_VIDEO_BASE_URL
-} from "./youtube-video";
--- src/app/shared/apis/youtube-video.ts
+++
@@ -1,3 +0,0 @@
-export const YTB_API_KEY = 'AIzaSyCss8hgggxG2xlH3Qa9iQe-tNIJIyDEM10';
-export const YTB_API_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
-export const YTB_VIDEO_BASE_URL = 'http://www.youtube.com/embed/';
