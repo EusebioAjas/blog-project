8fa118911f3c5c4d3fde1d7690259ebbb16f5e92
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-24 11:57:44 -0500

    Add video frame component

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+import { YtbVideoFrameComponent } from "./ytb-video-frame/ytb-video-frame.component";
+
+export const componets: any [] = [
+  YtbVideoFrameComponent
+]
+
+export * from './ytb-video-frame/ytb-video-frame.component';
