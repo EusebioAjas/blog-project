d794a066812046b7bb62a318d622fae9caacf8e7
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-24 11:58:26 -0500

    Add ytb-video-frame to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { YtbVideoFrameComponent } from './components/ytb-video-frame/ytb-video-frame.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: YtbVideoFrameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovieCardComponent, NavbarComponent } from './shared/components';
 import { MovieCardDirective, NavbarDirective } from './shared/directives';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { YtbVideoFrameComponent } from './components';
 
 @NgModule({
   declarations: [
@@ -13,14 +16,22 @@ import { MovieCardDirective, NavbarDirective } from './shared/directives';
     NavbarComponent,
     NavbarDirective,
     MovieCardDirective,
-    MovieCardComponent
+    MovieCardComponent,
+    YtbVideoFrameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [NavbarComponent, NavbarDirective, MovieCardDirective, MovieCardComponent],
+  providers: [
+    NavbarComponent,
+    NavbarDirective,
+    MovieCardDirective,
+    MovieCardComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
