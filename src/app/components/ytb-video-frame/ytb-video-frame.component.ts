45c4dd0ceca864ad42be1356e257fb2935f51c92
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-28 09:40:10 -0500

    Rename busqueda by searchVideos

--- src/app/components/ytb-video-frame/ytb-video-frame.component.ts
+++ src/app/components/ytb-video-frame/ytb-video-frame.component.ts
@@ -1,34 +1,35 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { SearchVideo } from 'src/app/models';
 import { YouTubeVideoService } from 'src/app/services';
 import { YOUTUBE_BASE_URL } from 'src/app/config';
+import { Video } from 'src/app/models/video';
 
 @Component({
   selector: 'app-ytb-video-frame',
   templateUrl: './ytb-video-frame.component.html',
 })
 export class YtbVideoFrameComponent implements OnInit {
-  searchResult!: SearchVideo;
+  videos!: Video[];
   @Input() queryYtb!: string;
 
-  constructor(private ytbService: YouTubeVideoService, private _sanitizer: DomSanitizer) { 
-    this.busqueda();
-  }
+  constructor(
+    private ytbService: YouTubeVideoService,
+    private _sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
+    this.searchVideos();
   }
 
-  busqueda = () => {
-    this.ytbService.getYouTubeVideos(this.queryYtb)
-      .then((response) => {
-        this.searchResult = response;
-      }, (error) => {
-        alert("error: " + error.statusText)
-      });
+  searchVideos() {
+    this.ytbService
+      .searchYouTubeVideo(this.queryYtb)
+      .subscribe((videos) => (this.videos = videos.items));
   }
 
   getVideo(videoId: string) {
-    return this._sanitizer.bypassSecurityTrustResourceUrl(YOUTUBE_BASE_URL + videoId);
+    return this._sanitizer.bypassSecurityTrustResourceUrl(
+      YOUTUBE_BASE_URL + videoId
+    );
   }
 }
