169394e4590309a929f1f1cbfcc1310e57f422f4
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-27 19:51:49 -0500

    Add route for movie detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PopularMoviesComponent } from './components/popular-movies/popular-movies.component';
 import { YtbVideoFrameComponent } from './components/ytb-video-frame/ytb-video-frame.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
-  { path: 'search', component: YtbVideoFrameComponent }
+  { path: '', component: PopularMoviesComponent },
+  { path: 'detail/:id', component: MovieDetailComponent },
+  { path: 'search', component: YtbVideoFrameComponent },
 ];
 
 @NgModule({
