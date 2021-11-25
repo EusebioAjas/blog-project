11472ce7f4a05e6067bdd3d9b9275f59ea3ad65b
VictorWars <cauichdavalosvictorenrique@gmail.com>
2021-11-25 02:32:58 -0600

    Finished register view design
    - finished register view design
    - Added JQuery
    - Added checkbox validation

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -22,6 +23,8 @@ export class NavbarComponent implements OnInit {
   });
 
   loginModal(contenido:any){
+    this.modal.dismissAll();
+
     this.modal.open(contenido,{
       windowClass:'modal-login'
     })
@@ -35,9 +38,34 @@ export class NavbarComponent implements OnInit {
     this.modal.open(contenido,{
       windowClass:'modal-register'
     })
-
-    this.formularioDeIngreso.get('username')?.touched
   }
 
+  formularioDeRegistro = new FormGroup({
+    username: new FormControl('',Validators.required),
+    userpassword: new FormControl('',Validators.required),
+    useremail: new FormControl('',Validators.required),
+    usernews: new FormControl('',Validators.required),
+    useractioncategory: new FormControl('',Validators.required)
+  });
 
+  activarCategorias() {
+    if($('.newsprincipal').is(':checked')){
+      $('#categories').css('display','block');
+    }else{
+      $('#categories').css('display','none');
+    }
+  }
+
+  registrar() {
+    if($('.newsprincipal').is(':checked')){
+      if($('.checkcategories').is(':checked')){
+        $('.alertcategories').css('display','none');
+        alert("Seleccionaste al menos una categoria, ciudadano.");
+      }else{
+        $('.alertcategories').css('display','block');
+      }
+    }else{
+      alert("No seleccionaste que quieres recibir novedades, entendido ciudadano.");
+    }
+  }
 }
