0a8502638c8cb770b8503b3858e89abc3bd852e8
VictorWars <cauichdavalosvictorenrique@gmail.com>
2021-11-26 02:50:41 -0600

    Obtaining inputs
    - Obtaining inputs
    - Input fill validator
    - Get checkbox

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
-  encapsulation:ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
@@ -14,59 +14,103 @@ export class NavbarComponent implements OnInit {
   constructor(private modal: NgbModal) { }
 
   ngOnInit(): void {
-    
+
   }
 
   formularioDeIngreso = new FormGroup({
-    username: new FormControl('',Validators.required),
-    userpassword: new FormControl('',Validators.required)
+    username: new FormControl('', Validators.required),
+    userpassword: new FormControl('', Validators.required)
   });
 
-  loginModal(contenido:any){
+  loginModal(contenido: any) {
     this.modal.dismissAll();
 
-    this.modal.open(contenido,{
-      windowClass:'modal-login'
+    this.modal.open(contenido, {
+      windowClass: 'modal-login'
     })
 
     this.formularioDeIngreso.get('username')?.touched
   }
 
-  RegisterModal(contenido:any){
+  RegisterModal(contenido: any) {
     this.modal.dismissAll();
 
-    this.modal.open(contenido,{
-      windowClass:'modal-register'
+    this.modal.open(contenido, {
+      windowClass: 'modal-register'
     })
   }
 
   formularioDeRegistro = new FormGroup({
-    username: new FormControl('',Validators.required),
-    userpassword: new FormControl('',Validators.required),
-    useremail: new FormControl('',Validators.required),
-    usernews: new FormControl('',Validators.required),
-    useractioncategory: new FormControl('',Validators.required)
+    username: new FormControl('', Validators.required),
+    userpassword: new FormControl('', Validators.required),
+    useremail: new FormControl('', Validators.required),
+    usernews: new FormControl('', Validators.required),
+    useractioncategory: new FormControl('', Validators.required)
   });
 
   activarCategorias() {
-    if($('.newsprincipal').is(':checked')){
+    if ($('.newsprincipal').is(':checked')) {
       $('#categories').show(1000);
-    }else{
+    } else {
       $('#categories').hide(1000);
     }
   }
 
+  login() {
+
+    if ($("#usernameLogin").val() != "" && $("#passwordLogin").val() != "") {
+      var username = $('#usernameLogin').val();
+      var password = $('#passwordLogin').val();
+
+      alert(username + "\n" + password);
+    } else {
+      alert("Acompleta");
+    }
+  }
+
   registrar() {
-    if($('.newsprincipal').is(':checked')){
-      if($('.checkcategories').is(':checked')){
+    var categories: Object[] = [];
+    var contador = 0;
+    var i = 1;
+    if ($('.newsprincipal').is(':checked')) {
+      if ($('.checkcategories').is(':checked')) {
         $('.alertcategories').hide();
-        alert("Seleccionaste al menos una categoria, ciudadano.");
-      }else{
+        if ($("#usernameRegister").val() != "" && $("#passwordRegister").val() != "" && $("#emailRegister").val() != "") {
+          var username = $('#usernameRegister').val();
+          var password = $('#passwordRegister').val();
+          var email = $('#emailRegister').val();
+          $("input[type=checkbox]:checked").each(function () {
+            if (i != 1) {
+              categories.push(this.id);
+              contador++;
+            } else {
+              i++;
+            }
+          });
+          alert(username + "\n" + password + "\n" + email + "\n" +
+            categories.map((categoria) => "name: " + categoria));
+          /*alert(username + "\n" + password + "\n" + email + "\n" + 
+          categories.map( categoria =>  ({name: categoria})));
+          /*for (i = 0; i < contador; i++){
+            alert(categories[i]);
+          }*/
+        } else {
+          alert("Acompleta");
+        }
+
+      } else {
         $('.alertcategories').css('color', '#FC1342');
         $('.alertcategories').show();
       }
-    }else{
-      alert("No seleccionaste que quieres recibir novedades, entendido ciudadano.");
+    } else {
+      if ($("#usernameRegister").val() != "" && $("#passwordRegister").val() != "" && $("#emailRegister").val() != "") {
+        var username = $('#usernameRegister').val();
+        var password = $('#passwordRegister').val();
+        var email = $('#emailRegister').val();
+        alert(username + "\n" + password + "\n" + email );
+      } else {
+        alert("Acompleta");
+      }
     }
   }
 }
