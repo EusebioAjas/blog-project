e6ba665cd8e9cb52aff20234ff4cbe1cf45b36ef
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-12-04 19:49:34 -0600

    Update comment service
    Co-authored-by: VictorWars <cauichdavalosvictorenrique@gmail.com>

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { API_BASE_URL } from '../config/api';
 import { Comment } from '../models/comment';
 
@@ -9,9 +11,30 @@ import { Comment } from '../models/comment';
 export class CommentService {
   constructor(private httpClient: HttpClient) {}
 
-  getComments(movieId: number) {
+  getComments(movieId: number): Observable<Comment[]> {
     return this.httpClient
-      .get<Comment[]>(`${API_BASE_URL}/movies/${movieId}/comments`)
-      .toPromise();
+      .get<Comment[]>(`${API_BASE_URL}/movies/${movieId}/comments`);
+  }
+
+  getUserComments(): Observable<Comment[]> {
+    return this.httpClient
+      .get<Comment[]>(`${API_BASE_URL}/users/comments`);
+  }
+
+  destroyUserComments(commentId: number): Observable<string> {
+    return this.httpClient
+      .delete<string>(`${API_BASE_URL}/users/comments/${commentId}`)
+      .pipe(tap((_) => alert('Deleted comment id=' + commentId)));
+  }
+
+  udpdateComment(commentId: number): Observable<string> {
+    return this.httpClient
+      .put<string>(`${API_BASE_URL}/users/comments/${commentId}`, commentId)
+      .pipe(tap((_) => alert('Updated comment id=' + commentId)));
+  }
+
+  getComment(commentId: number): Observable<Comment> {
+    return this.httpClient
+      .get<Comment>(`${API_BASE_URL}/users/comments/${commentId}`);
   }
 }
