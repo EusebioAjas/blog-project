6fc427177e9f1b9843e9086dcfadd32a29fcedea
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-12-05 23:26:21 -0600

    Add logOut route
    Co-authored-by: VictorWars <cauichdavalosvictorenrique@gmail.com>

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,35 +1,19 @@
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
-  encapsulation:ViewEncapsulation.None,
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
   public isCollapsed = true;
 
-  constructor(private modal: NgbModal) { }
+  constructor(private authService: AuthenticationService) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  logout() {
+    this.authService.logOut();
   }
-
-  formularioDeIngreso = new FormGroup({
-    username: new FormControl('',Validators.required),
-    userpassword: new FormControl('',Validators.required)
-  });
-
-  loginModal(contenido:any){
-    this.modal.open(contenido,{
-      windowClass:'modal-login'
-    })
-
-    this.formularioDeIngreso.get('username')?.touched
-  }
-
-
 }
